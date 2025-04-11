b29207590a92b8400f50fd632f2c9cae6948c33e
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-04-10 21:32:45 -0700

    Add sh script to download config

---
+++ scripts/download_config.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+NIX_CONFIG_PATH='/etc/nixos/configuration.nix'
+
+# download config
+curl \
+  --show-error \
+  --fail \
+  https://raw.githubusercontent.com/tibtiq/nix-config-public/refs/heads/main/proxmox_lxc_config.nix \
+  >${NIX_CONFIG_PATH}
+
+# add ssh keys from github
+GITHUB_USERNAME='tibtiq'
+SSH_KEYS=$(curl -s https://github.com/${GITHUB_USERNAME}.keys | sed 's/.*/        "&",/')
+sed -i '/"SSH_KEYS"/{
+    r /dev/stdin
+    d
+}' ${NIX_CONFIG_PATH} <<EOF
+"$SSH_KEYS"
+EOF
+# fix issues with previous sed
+sed -i 's/\"        \"/        \"/' ${NIX_CONFIG_PATH}
+sed -i 's/\",\"/\",/' ${NIX_CONFIG_PATH}

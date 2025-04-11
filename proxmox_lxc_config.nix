306f20e67b05a85706b874e6bd94885f530c9f7d
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-04-10 20:22:05 -0700

    Add working proxmox lxc nix config
    this too

---
+++ proxmox_lxc_config.nix
@@ -0,0 +1,90 @@
+{
+  modulesPath,
+  config,
+  pkgs,
+  inputs,
+  ...
+}: let
+  hostname = "nixos";
+  user = "tibtiq";
+  password = "somepass";
+
+  timeZone = "America/New_York";
+  defaultLocale = "en_US.UTF-8";
+in {
+  imports = [
+    # Include the default lxc/lxd configuration.
+    "${modulesPath}/virtualisation/lxc-container.nix"
+  ];
+
+  boot.isContainer = true;
+  networking.hostName = hostname;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    tmux
+  ];
+
+  time.timeZone = timeZone;
+
+  i18n = {
+    defaultLocale = defaultLocale;
+    extraLocaleSettings = {
+      LC_ADDRESS = defaultLocale;
+      LC_IDENTIFICATION = defaultLocale;
+      LC_MEASUREMENT = defaultLocale;
+      LC_MONETARY = defaultLocale;
+      LC_NAME = defaultLocale;
+      LC_NUMERIC = defaultLocale;
+      LC_PAPER = defaultLocale;
+      LC_TELEPHONE = defaultLocale;
+      LC_TIME = defaultLocale;
+    };
+  };
+
+  users = {
+    mutableUsers = false;
+    users."${user}" = {
+      isNormalUser = true;
+      password = password;
+      openssh.authorizedKeys.keys = [
+        "SSH_KEYS"
+      ];
+      extraGroups = ["wheel"];
+    };
+  };
+
+  # Enable passwordless sudo.
+  security.sudo.extraRules = [
+    {
+      users = [user];
+      commands = [
+        {
+          command = "ALL";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+
+  # Supress systemd units that don't work because of LXC.
+  # https://blog.xirion.net/posts/nixos-proxmox-lxc/#configurationnix-tweak
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      PasswordAuthentication = false;
+    };
+  };
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  system.stateVersion = "24.05";
+}

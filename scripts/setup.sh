d794faf50346f72291c4f0890d73550a0a5295e5
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-04-12 12:39:09 -0700

    Revert "fix issue with read not running"
    This reverts commit dd16c041b68ec2a8ac4cb52c8bcb867476dd4c62.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,33 +1,26 @@
 #!/bin/sh
-set -e
-
 NIX_CONFIG_PATH='/etc/nixos/configuration.nix'
 
-# Download config
-curl --show-error --fail \
+# download config
+curl \
+  --show-error \
+  --fail \
   https://raw.githubusercontent.com/tibtiq/nix-config-public/refs/heads/main/proxmox_lxc_config.nix \
-  >"$NIX_CONFIG_PATH"
+  >${NIX_CONFIG_PATH}
 
-# Add SSH keys from GitHub
+# add ssh keys from github
 GITHUB_USERNAME='tibtiq'
-SSH_KEYS=$(curl -s "https://github.com/${GITHUB_USERNAME}.keys" | sed 's/.*/        "&"/')
+SSH_KEYS=$(curl -s https://github.com/${GITHUB_USERNAME}.keys | sed 's/.*/        "&"/')
+echo "$SSH_KEYS" | sed -i '/"SSH_KEYS"/{
+    r /dev/stdin
+    d
+}' "${NIX_CONFIG_PATH}"
+# fix issues with previous sed
+sed -i 's/\"        \"/        \"/' ${NIX_CONFIG_PATH}
+sed -i 's/\"\"/\"/' ${NIX_CONFIG_PATH}
 
-# Replace the "SSH_KEYS" line with the actual keys
-# We write a temporary file to insert cleanly
-TMPFILE=$(mktemp)
-
-# Copy everything up to the match
-awk '/"SSH_KEYS"/ { exit } { print }' "$NIX_CONFIG_PATH" >"$TMPFILE"
-# Add the SSH keys
-printf "%s\n" "$SSH_KEYS" >>"$TMPFILE"
-# Copy everything after the match
-awk '/"SSH_KEYS"/ { found=1; next } found { print }' "$NIX_CONFIG_PATH" >>"$TMPFILE"
-
-# Move back
-mv "$TMPFILE" "$NIX_CONFIG_PATH"
-
-# Prompt user for rebuild
-printf "Do you want to rebuild Nix? (y/N): "
+# todo fix this prompt. the script isn't waiting
+printf "Do you want to rebuild nix? (y/N): "
 read -r answer
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
   sudo nix-channel --update && sudo nixos-rebuild switch --upgrade
